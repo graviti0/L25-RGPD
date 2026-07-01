c1fdbbd9f83ee31ac7e83a7cc974addf5b8f6d82
Pierre_desktop <maffre.pierre@orange.fr>
2026-07-01 22:39:48 +0200

    Install django + django_tailwind_cli

---
+++ Conformity/poll/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def landing_page(request):
+    return render(request, "landing_page.html")
+
+# Create your views here.
